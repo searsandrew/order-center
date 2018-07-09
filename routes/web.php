f7e4347108df035ca2244c3b529f11c02cbdc85c
Andrew Sears <andrew@mayfifteenth.com>
2018-07-08 23:40:12 -0500

    Added order routes with gates

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,28 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'OrderController@index');
+Route::get('/orders', 'OrderController@index')->name('list_orders');
+Route::group(['prefix' => 'orders'], function () {
+    Route::get('/drafts', 'OrderController@drafts')
+        ->name('list_orders')
+        ->middleware('auth');
+    Route::get('/show/{id}', 'OrderController@show')
+        ->name('show_order');
+    Route::get('/create', 'OrderController@create')
+        ->name('create_order')
+        ->middleware('can:create-order');
+    Route::post('/create', 'OrderController@store')
+        ->name('store_order')
+        ->middleware('can:create-order');
+    Route::get('/edit/{order}', 'OrderController@edit')
+        ->name('edit_order')
+        ->middleware('can:update-order,order');
+    Route::post('/edit/{order}', 'OrderController@update')
+        ->name('update_order')
+        ->middleware('can:update-order,order');
+    Route::get('/publish/{order}', 'OrderController@publish')
+        ->name('publish_order')
+        ->middleware('can:publish-order');
+});

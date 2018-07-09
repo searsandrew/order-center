9236f3a231cbd849889215df7cab87d9c1d583f4
Andrew Sears <andrew@mayfifteenth.com>
2018-07-08 22:25:06 -0500

    Add pivot table and seeder

---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $customer = Role::create([
+            'name' => 'Customer',
+            'slug' => 'customer',
+            'permissions' => [
+                'create-order' => true,
+            ]
+        ]);
+        $employee = Role::create([
+            'name' => 'Employee',
+            'slug' => 'employee',
+            'permissions' => [
+                'update-order' => true,
+                'publish-order' => true,
+            ]
+        ]);
+    }
+}

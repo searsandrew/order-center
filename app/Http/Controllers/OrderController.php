b332628961523099c6023f5c7d31545e4fb3296d
Andrew Sears <andrew@mayfifteenth.com>
2018-07-08 23:51:55 -0500

    Add Order model & start Order Controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Order;
+
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+        $orders = Order::completed()->paginate();
+        return view('orders.index', compact('orders'));
+    }
 }
--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'slug', 'body', 'user_id',
+    ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeCompleted($query)
+    {
+        return $query->where('completed', true);
+    }
+
+    public function scopeUncompleted($query)
+    {
+        return $query->where('completed', false);
+    }
 }

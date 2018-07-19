eee0710ad1014cf69874dc0f189d713bb355f9c9
Andrew Sears <andrew@mayfifteenth.com>
2018-07-19 18:26:15 -0500

    Updating some Bootstrap and going to Flexbox

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -2,53 +2,54 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">New Order</div>
+    <div class="d-flex justify-content-center">
+        <div class="col-sm-10 col-md-8 col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Create a New Order</h3>
+                    <div class="card-text">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ route('store_order') }}">
+                            {{ csrf_field() }}
 
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('store_order') }}">
-                        {{ csrf_field() }}
+                            <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+                                <label for="title" class="col-md-4 control-label">Order Name</label>
 
-                        <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-                            <label for="title" class="col-md-4 control-label">Title</label>
+                                <div class="col-md-12">
+                                    <input id="title" type="text" class="form-control" name="title" value="{{ old('title') }}" required autofocus>
 
-                            <div class="col-md-6">
-                                <input id="title" type="text" class="form-control" name="title" value="{{ old('title') }}" required autofocus>
-
-                                @if ($errors->has('title'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('title') }}</strong>
-                                    </span>
-                                @endif
+                                    @if ($errors->has('title'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('title') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('body') ? ' has-error' : '' }}">
-                            <label for="body" class="col-md-4 control-label">Body</label>
+                            <div class="form-group{{ $errors->has('body') ? ' has-error' : '' }}">
+                                <label for="body" class="col-md-4 control-label">Order Details</label>
 
-                            <div class="col-md-6">
-                                <textarea name="body" id="body" cols="30" rows="10" class="form-control" required>{{ old('body') }}</textarea>
-                                @if ($errors->has('body'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('body') }}</strong>
-                                    </span>
-                                @endif
+                                <div class="col-md-12">
+                                    <textarea name="body" id="body" cols="30" rows="10" class="form-control" required>{{ old('body') }}</textarea>
+                                    @if ($errors->has('body'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('body') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Create
-                                </button>
-                                <a href="{{ route('list_orders') }}" class="btn btn-default">
-                                    Cancel
-                                </a>
+                            <div class="form-group">
+                                <div class="col-md-12 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Create Order
+                                    </button>
+                                    <a href="{{ route('list_orders') }}" class="btn btn-default">
+                                        Cancel
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
